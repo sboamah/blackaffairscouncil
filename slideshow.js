eb14e24977001f07123fd5fa5e21b81c9cc8db6f
sboamah <sboamah@stevens.edu>
2020-05-02 16:36:29 -0400

    git commit -m "new files" git config user.email "sboamah@stevens.edu" git config user.name "sboamah" git commit -m "new files"

---
+++ slideshow.js
@@ -0,0 +1,18 @@
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
